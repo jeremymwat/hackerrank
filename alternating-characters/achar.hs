94fd0eba30da402c0f854e1c6ea31d29ea90ab1b
Jeremy W <jeremymwatson@gmail.com>
2015-01-13 12:23:02 -0500

    finish alt char

---
+++ alternating-characters/achar.hs
@@ -0,0 +1,17 @@
+-- Enter your code here. Read input from STDIN. Print output to STDOUT
+import Control.Monad
+
+chew :: String -> Int -> Int
+chew str acc = if str == [] || tail str == [] then acc 
+                else let (a,b) = (head str, head $ tail str) in
+                    if a == b then chew (tail str) (acc + 1)
+                        else chew (tail str) acc
+                                
+
+main :: IO ()
+main = do
+    n <- readLn :: IO Int
+    lineList <- replicateM n getLine
+    let
+        ans = map (\s -> chew s 0) lineList
+    mapM_ print ans
