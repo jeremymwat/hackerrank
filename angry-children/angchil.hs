67d83e4eda477907ee75ad0e99baffb4a12d906c
jeremy <jeremy@watsonly.com>
2015-01-13 15:02:12 -0500

    giving up on haskell for angry chidlren

--- angry-children/angchil.hs
+++ angry-children/angchil.hs
@@ -3,10 +3,11 @@ import Control.Monad
 import Data.List
 
 fair :: [Int] -> Int -> Int
-fair lst k = let m = maximum lst in
-    let fair_helper lstH kH mH = if length (take kH lstH) < k then mH 
-                        else fair_helper (tail lstH) kH 
-                            (minimum [mH,maximum (take kH lstH) - minimum (take kH lstH)])
+fair lst k = let m = lst!!(length lst) in
+    let fair_helper lstH kH mH = let newL = take kH lstH in
+                        if length newL < k then mH 
+                        else let (minH, maxH) = (head newL, last newL) in 
+                        fair_helper (tail lstH) kH (minimum [mH,maxH-minH])
      in fair_helper lst k m
 
 
